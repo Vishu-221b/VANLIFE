3a2a096c36320ea4f9820834655c337699fdd644
Vishu <Vishwajeetsinghchauhan63@gmail.com>
2023-08-03 11:10:51 +0530

    few changes

--- src/index.jsx
+++ src/index.jsx
@@ -26,7 +26,5 @@ function About() {
   )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<StrictMode>
-  <App />
-</StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
